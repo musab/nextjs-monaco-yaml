247d03e1e3bf9875ed385ad0b2a919bdaa9201a1
Tyler Sullberg <tysullberg@ip-192-168-196-24.us-west-1.compute.internal>
2020-11-20 13:17:10 -0800

    Added working json example

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
-import sample from '../code-sample'
+import sample from "../code-sample3";
 
-const MonacoEditor = dynamic(import('react-monaco-editor'), { ssr: false })
+const MonacoEditor = dynamic(import("react-monaco-editor"), { ssr: false });
 
 function IndexPage() {
   return (
     <MonacoEditor
-      height={'600px'}
-      language="typescript"
+      height={"600px"}
+      language="json"
       theme="vs-dark"
       value={sample}
       onChange={console.log}
       editorDidMount={() => {
         window.MonacoEnvironment.getWorkerUrl = (moduleId, label) => {
-          if (label === 'json') return '/_next/static/json.worker.js'
-          if (label === 'css') return '/_next/static/css.worker.js'
-          if (label === 'html') return '/_next/static/html.worker.js'
-          if (label === 'typescript' || label === 'javascript')
-            return '/_next/static/ts.worker.js'
-          return '/_next/static/editor.worker.js'
-        }
+          console.log(
+            "setting MonacoEnvironment.moduleID is ",
+            moduleId,
+            "and label is ",
+            label
+          );
+          if (label === "json") return "/_next/static/json.worker.js";
+          if (label === "css") return "/_next/static/css.worker.js";
+          if (label === "html") return "/_next/static/html.worker.js";
+          if (label === "typescript" || label === "javascript")
+            return "/_next/static/ts.worker.js";
+          return "/_next/static/editor.worker.js";
+        };
       }}
     />
-  )
+  );
 }
 
-export default IndexPage
+export default IndexPage;
